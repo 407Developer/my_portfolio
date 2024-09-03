08cf588be3ac692a7b4a7187bf9194200335a49a
Chukwudi Divine <407developer@gmail.com>
2024-09-03 04:10:39 +0100

    edited placeholders and changed the font to be like brittany chang's V4

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f82f2094fe5a16187ef6cee0dd3bea75750c0bcb83d84f701d4d93ed22c0bfd
-size 7151
+oid sha256:8f0c1419a8bcdae90d44887294c725d766770fbd204f5eb8dde73008550c92dc
+size 7154
